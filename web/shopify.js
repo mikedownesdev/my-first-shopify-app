96f3eb5801114b62750e3d78eee8801ad1c67764
Mike Downes <mikedownes.dev@gmail.com>
2023-01-13 14:20:51 -0500

    backend-tutorial complete

--- web/shopify.js
+++ web/shopify.js
@@ -2,8 +2,8 @@ import { BillingInterval, LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApp } from "@shopify/shopify-app-express";
 import { SQLiteSessionStorage } from "@shopify/shopify-app-session-storage-sqlite";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-01";
-
-const DB_PATH = `${process.cwd()}/database.sqlite`;
+import { join } from "path";
+import { QRCodesDB } from "./qr-codes-db.js";
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
@@ -16,6 +16,11 @@ const billingConfig = {
   },
 };
 
+const dbFile = join(process.cwd(), "database.sqlite");
+const sessionDb = new SQLiteSessionStorage(dbFile);
+// Initialize SQLite DB
+QRCodesDB.db = sessionDb.db;
+QRCodesDB.init();
 const shopify = shopifyApp({
   api: {
     apiVersion: LATEST_API_VERSION,
@@ -29,8 +34,7 @@ const shopify = shopifyApp({
   webhooks: {
     path: "/api/webhooks",
   },
-  // This should be replaced with your preferred storage strategy
-  sessionStorage: new SQLiteSessionStorage(DB_PATH),
+  sessionStorage: sessionDb,
 });
 
 export default shopify;
